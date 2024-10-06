c1fb2063e913e6c9ba1c1e44d8230cfce4538586
Dandelion Lowcode <fmartinez@uground.com>
2024-10-07 00:50:59 +0200

    Convert to JavaPropertyGetter and JavaPropertySetter

--- org.eclipse.epsilon.emc.structurizr/src/org/eclipse/epsilon/emc/structurizr/StructurizrPropertyGetter.java
+++ org.eclipse.epsilon.emc.structurizr/src/org/eclipse/epsilon/emc/structurizr/StructurizrPropertyGetter.java
@@ -10,53 +10,31 @@
  ******************************************************************************/
 package org.eclipse.epsilon.emc.structurizr;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
-import org.eclipse.epsilon.eol.execute.introspection.AbstractPropertyGetter;
+import org.eclipse.epsilon.eol.execute.introspection.java.JavaPropertyGetter;
 
 import com.structurizr.PropertyHolder;
 
-public class StructurizrPropertyGetter extends AbstractPropertyGetter {
+public class StructurizrPropertyGetter extends JavaPropertyGetter {
 
 	@Override
 	public Object invoke(Object object, String property, IEolContext context) throws EolRuntimeException {
-		if ("name".equals(property)) {
-			return invokeGetterOrThrow(object, "getName", property);
-		} else if ("description".equals(property)) {
-			return invokeGetterOrThrow(object, "getDescription", property);
-		} else if ("tag".equals(property)) {
-			return invokeGetterOrThrow(object, "getTag", property);
-		}
-
-		if (!(object instanceof PropertyHolder)) {
-			throw new EolRuntimeException(
-					"Cannot get property " + property + " of non-PropertyHolder object " + object);
-		}
-
-		PropertyHolder propertyHolder = (PropertyHolder) object;
-		if (!propertyHolder.getProperties().containsKey(property)) {
-			throw new EolRuntimeException("Property " + property + " not found in PropertyHolder " + propertyHolder);
-		}
-
-		return propertyHolder.getProperties().get(property);
-	}
-
-	private Object invokeGetterOrThrow(Object target, String methodName, String property) throws EolRuntimeException {
 		try {
-			Method method = target.getClass().getMethod(methodName);
-			return method.invoke(target);
-		} catch (NoSuchMethodException e) {
-			throw new EolRuntimeException(
-					"The getter " + methodName + " does not exist on class " + target.getClass().getSimpleName());
-		} catch (IllegalAccessException e) {
-			throw new EolRuntimeException(
-					"Cannot access the getter " + methodName + " on class " + target.getClass().getSimpleName());
-		} catch (InvocationTargetException e) {
-			throw new EolRuntimeException(
-					"Cannot invoke the getter " + methodName + " on class " + target.getClass().getSimpleName());
+			return super.invoke(object, property, context);
+		} catch (EolRuntimeException e) {
+			if (!(object instanceof PropertyHolder)) {
+				throw new EolRuntimeException(
+						"Cannot get property " + property + " of non-PropertyHolder object " + object);
+			}
+
+			PropertyHolder propertyHolder = (PropertyHolder) object;
+			if (!propertyHolder.getProperties().containsKey(property)) {
+				throw new EolRuntimeException(
+						"Cannot find property " + property + " in PropertyHolder " + propertyHolder);
+			}
+			return propertyHolder.getProperties().get(property);
 		}
+
 	}
 }
--- org.eclipse.epsilon.emc.structurizr/src/org/eclipse/epsilon/emc/structurizr/StructurizrPropertySetter.java
+++ org.eclipse.epsilon.emc.structurizr/src/org/eclipse/epsilon/emc/structurizr/StructurizrPropertySetter.java
@@ -10,59 +10,31 @@
  ******************************************************************************/
 package org.eclipse.epsilon.emc.structurizr;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
-import org.eclipse.epsilon.eol.execute.introspection.AbstractPropertySetter;
+import org.eclipse.epsilon.eol.execute.introspection.java.JavaPropertySetter;
 
 import com.structurizr.PropertyHolder;
 
-public class StructurizrPropertySetter extends AbstractPropertySetter {
+public class StructurizrPropertySetter extends JavaPropertySetter {
 
 	@Override
 	public void invoke(Object target, String property, Object value, IEolContext context) throws EolRuntimeException {
-		if (!(value instanceof String)) {
-			throw new EolRuntimeException("Cannot set property " + property + " to a non-String value " + value);
-		}
-
-		String stringValue = (String) value;
-
-		if ("name".equals(property)) {
-			invokeSetterOrThrow(target, "setName", stringValue, property);
-			return;
-		} else if ("description".equals(property)) {
-			invokeSetterOrThrow(target, "setDescription", stringValue, property);
-			return;
-		} else if ("tag".equals(property)) {
-			invokeSetterOrThrow(target, "setTag", stringValue, property);
-			return;
-		}
-
-		if (!(target instanceof PropertyHolder)) {
-			throw new EolRuntimeException(
-					"Cannot set property " + property + " of non-PropertyHolder object " + target);
-		}
-
-		PropertyHolder targetPropertyHolder = (PropertyHolder) target;
-		targetPropertyHolder.addProperty(property, stringValue);
-	}
-
-	private void invokeSetterOrThrow(Object target, String methodName, String value, String property)
-			throws EolRuntimeException {
 		try {
-			Method method = target.getClass().getMethod(methodName, String.class);
-			method.invoke(target, value);
-		} catch (NoSuchMethodException e) {
-			throw new EolRuntimeException(
-					"The setter " + methodName + " does not exist on class " + target.getClass().getSimpleName());
-		} catch (IllegalAccessException e) {
-			throw new EolRuntimeException(
-					"Cannot access the setter " + methodName + " on class " + target.getClass().getSimpleName());
-		} catch (InvocationTargetException e) {
-			throw new EolRuntimeException(
-					"Cannot invoke the setter " + methodName + " on class " + target.getClass().getSimpleName());
+			super.invoke(target, property, value, context);
+		} catch (EolRuntimeException e) {
+			if (!(target instanceof PropertyHolder)) {
+				throw new EolRuntimeException(
+						"Cannot set property " + property + " of non-PropertyHolder object " + target);
+			}
+
+			if (!(value instanceof String)) {
+				throw new EolRuntimeException("Cannot set property " + property + " to a non-String value");
+			}
+
+			String stringValue = (String) value;
+			PropertyHolder targetPropertyHolder = (PropertyHolder) target;
+			targetPropertyHolder.addProperty(property, stringValue);
 		}
 	}
 }
